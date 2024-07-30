6509b7e70096fa2414170cf63fd7c8cf763af335
jcasanova1990 <jeremykcasanova@gmail.com>
2024-07-30 11:33:25 -0400

    first

---
+++ BooleanOperators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 5;
+
+    cout << boolalpha << (a == b) << endl;
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "test oh yeah baby print me" << endl;
+
+    return 0;
+}
